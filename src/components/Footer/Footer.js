a91fa4595674248ec0da7701c1af6e1328e1f96f
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-10-11 22:08:28 -0700

    +routes
    installed react router
    
    inclided planned features as routes

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+// import './Footer.css';
+
+function Footer() {
+    return (
+      <div className="navbar">
+        Github
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+// import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div>WorldState</div>
+      <div>Plains of Eidelon | Orb Vallis | Cambion Drift</div>
+    </div>
+  );
+}
+
+export default Navbar;
