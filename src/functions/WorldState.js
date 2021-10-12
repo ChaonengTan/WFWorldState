5231ed873db9c5304102b6abbe681bdf676702eb
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-10-12 12:09:26 -0700

    correctly parse errors

--- src/functions/WorldState.js
+++ src/functions/WorldState.js
@@ -2,6 +2,6 @@ export default async function WorldState(platform, location) {
     const path = `https://api.warframestat.us/${platform}/${location}`
     const res = await fetch(path)
     const json = await res.json()
-    return json.cod!==200 ? {cod:json.cod, message:json.message} : await json
+    return json.error ? {cod:json.code, message:json.error} : await json
     
 }
