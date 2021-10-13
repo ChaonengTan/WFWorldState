9f9d223c5fb3cc0dff93c7c3f1c669f561a9c901
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-10-13 12:03:43 -0700

    +navbarStyle

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import { NavLink } from 'react-router-dom'
-// import './Navbar.css';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
       <div>
-        <NavLink to="/">WFWorldState</NavLink>
+        <NavLink className='navLink' to="/">WFWorldState</NavLink>
       </div>
-      <div>
-        <NavLink to="/PoE">Plains of Eidelon</NavLink>
-        <NavLink to="/OV">Orb Vallis</NavLink>
-        <NavLink to="/CD">Cambion Drift</NavLink>
+      <div className='navRight'>
+        <NavLink className='navLink' to="/PoE">Plains of Eidelon</NavLink>
+        <NavLink className='navLink' to="/OV">Orb Vallis</NavLink>
+        <NavLink className='navLink' to="/CD">Cambion Drift</NavLink>
       </div>
     </div>
   );
