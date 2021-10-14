71577270dc230389f4e31caa3c99e54106e827d1
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-10-14 12:51:14 -0700

    +PlainsOfEidelon Route
    implemented page for PlainsOfEidelon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import WFWorldState from './components/WFWorldState/WFWorldState';
+import PlainsOfEidelon from './components/PlainsOfEidelon/PlainsOfEidelon';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Navbar/>
         <Route exact path="/" component={WFWorldState}/>
-        {/* <Route path="/PoE" component={PlainsOfEidelon}/> */}
+        <Route path="/PoE" component={PlainsOfEidelon}/>
         {/* <Route path="/OV" component={OrbVallis}/> */}
         {/* <Route path="/CD" component={CambionDrift}/> */}
         <Footer/>
---
+++ src/components/PlainsOfEidelon/PlainsOfEidelon.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { getData } from "../../functions/WorldState";
+import useInterval from '../../functions/useInterval';
+import parseTime from '../parseTime';
+import '../WFWorldState/WFWorldState.css'
+import './PlainsOfEidelon.css';
+
+function PlainsOfEidelon() {
+    const [earth, setEarth] = useState(null)
+    const [cetus, setCetus] = useState(null)
+    useInterval(() => {
+        getData(setEarth, `earth`)
+        getData(setCetus, `cetus`)
+    }, 1000)
+    return (
+        <div>
+            {earth && cetus ?
+            <div className="wfWorldState">
+                <div className='earth'>
+                    <div>
+                        <div>Earth | {earth.state}</div>
+                        {parseTime(earth.timeLeft)}
+                    </div>
+                    <div>
+                        <div>Cetus | {cetus.state}</div>
+                        {parseTime(cetus.timeLeft)}
+                    </div>
+                </div>
+            </div> :
+            <div className='loading'>Fetching Data</div>
+            }
+        </div>
+    );
+}
+
+export default PlainsOfEidelon;
