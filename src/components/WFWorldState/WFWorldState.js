332a8820fd54009c062bb9deb5332e8964650a7b
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-10-14 12:01:48 -0700

    parseData
    makes stuff easier to read

--- src/components/WFWorldState/WFWorldState.js
+++ src/components/WFWorldState/WFWorldState.js
@@ -1,18 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import WorldState from "../../functions/WorldState";
 import useInterval from '../../functions/useInterval';
 import './WFWorldState.css';
 
 function WFWorldState() {
     const [data, setData] = useState(null)
-    async function getData() {
+    async function getAllData(setData) {
         const earth = await WorldState('pc', 'earthCycle')
         const cetus = await WorldState('pc', 'cetusCycle')
         const vallis = await WorldState('pc', 'vallisCycle')
         const cambion = await WorldState('pc', 'cambionCycle')
         Promise.all([earth, cetus, vallis, cambion]).then(() => {
+            // Gives cambionDrift a timeLeft attribute
             cambion.timeLeft = cetus.timeLeft
         }).then(() => {
+            // Manually calculates time left from expiryTime-currentTime
             [earth, cetus, vallis, cambion].forEach(loc => {
                 const currentTime = new Date()
                 const expTime = new Date(loc.expiry)
@@ -20,14 +22,20 @@ function WFWorldState() {
                 // console.log(`difference: ${(expTime-currentTime)/1000}s`)
             })
         }).then(() => {
+            // Capitalizes states ie. day, warm, fass
+            [earth, cetus, vallis].forEach(loc => {
+                loc.state = loc.state.charAt(0).toUpperCase() + loc.state.slice(1)
+            })
+            cambion.active = cambion.active.charAt(0).toUpperCase() + cambion.active.slice(1)
+        }).then(() => {
             setData({earth,cetus,vallis,cambion})
         })
     }
-    // useInterval(getData, 1000)
-    useEffect(() => {
-        getData()
-    }, []);
-    console.log(data)
+    useInterval(() => {getAllData(setData)}, 1000)
+    // useEffect(() => {
+    //     getAllData()
+    // }, []);
+    // console.log(data)
     function parseTime(timeLeft) {
         return (
             <div>
@@ -41,24 +49,20 @@ function WFWorldState() {
             <div className="wfWorldState">
                 <div className='earth'>
                     <div>
-                        earth:
-                        {data.earth.state}
+                        <div>Earth | {data.earth.state}</div>
                         {parseTime(data.earth.timeLeft)}
                     </div>
                     <div>
-                        cetus:
-                        {data.cetus.state}
+                        <div>Cetus | {data.cetus.state}</div>
                         {parseTime(data.cetus.timeLeft)}
                     </div>
                 </div>
                 <div className='vallis'>
-                    orbVallis:
-                    {data.vallis.state}
+                    <div>Orb Vallis | {data.vallis.state}</div>
                     {parseTime(data.vallis.timeLeft)}
                 </div>
                 <div className='cambion'>
-                    cambionDrift:
-                    {data.cambion.active}
+                    <div>Cambion Drift | {data.cambion.active}</div>
                     {parseTime(data.cambion.timeLeft)}
                 </div>
             </div> :
