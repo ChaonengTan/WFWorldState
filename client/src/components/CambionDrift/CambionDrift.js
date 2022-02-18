c1def739e41abba28b570dfb7e83becb4ebdf53f
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-17 17:14:43 -0800

    cambionDrift from useQuery

--- client/src/components/CambionDrift/CambionDrift.js
+++ client/src/components/CambionDrift/CambionDrift.js
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import { getData } from "../../functions/WorldState";
-import useInterval from '../../functions/useInterval';
 import parseTime from '../parseTime';
-import timeLeft from '../timeLeft';
 import '../WFWorldState/WFWorldState.css'
 import '../PlainsOfEidelon/PlainsOfEidelon.css';
 
-function CambionDrift() {
-    const [cambion, setCambion] = useState(null)
-    const [, updateState] = useState(null)
-    useInterval(() => {
-        getData(setCambion, `cambion`)
-        updateState({})
-    }, 1000)
+import { useQuery } from '@apollo/client'
+import GetWorldState from '../../functions/queries/WorldStateQuery'
+
+export default function CambionDrift() {
+    const { data } = useQuery(
+        GetWorldState,
+        { 
+          variables: { location: 'cambionCycle'},
+          pollInterval: 1000
+        }
+    )
     return (
         <div>
-            {cambion ?
+            {data ?
             <div className="wfWorldState worldState">
                 <div className='cambion'>
-                    <div>Cambion Drift | {cambion.state}</div>
-                    {parseTime(timeLeft(cambion.expiry))}
+                    <div>Cambion Drift | {data.getWorldstate.state}</div>
+                    {parseTime(data.getWorldstate.timeLeft)}
                 </div>
             </div> :
             <div className='loading'>Fetching Data</div>
@@ -28,4 +28,3 @@ function CambionDrift() {
     );
 }
 
-export default CambionDrift;
